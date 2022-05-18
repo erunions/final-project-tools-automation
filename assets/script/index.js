6ff5547939524e7b3282a06c02e1748fc75c03b0
Jaeden Fuller <fuller.jc@outlook.com>
2022-05-18 13:52:54 -0500

    modal design finished

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,7 @@
 const downarrow = document.querySelector('.down');
 const modal = document.querySelector('dialog');
+const accept = document.getElementById('close')
+const manage = document.getElementById('manage')
 
 downarrow.addEventListener('click', function() {
     window.scrollBy(0, document.body.scrollHeight)
@@ -8,3 +10,7 @@ downarrow.addEventListener('click', function() {
 setTimeout(function() {
     modal.showModal();
 }, 1000)
+
+accept.addEventListener('click', function() {
+    modal.close();
+})
